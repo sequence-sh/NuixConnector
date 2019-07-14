65431eea36f86ac7eb48e2e10d1a1a2a8d4bdbda
MarkWainwright <Mark@reduc.tech>
2019-07-14 14:26:58 +0000

    Fixed a concurrency issue in Nuix Client Console

--- NuixClientConsole/Program.cs
+++ NuixClientConsole/Program.cs
@@ -18,8 +18,13 @@ namespace NuixClientConsole
 
             try
             {
-                while (enumerator.MoveNextAsync().Result)
+                while (true)
                 {
+                    var nextTask = enumerator.MoveNextAsync().AsTask();// .ConfigureAwait(false).GetAwaiter().GetResult();// .AsTask().aw.Wait();
+                    nextTask.Wait();
+                    var next = nextTask.Result;
+                    if (!next)
+                        break;
                     Console.WriteLine(enumerator.Current);
                 }
             }
