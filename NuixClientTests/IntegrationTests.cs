c40951ea9c53e74d734f7d8e6cd9fb71ae3efc4f
Mark Wainwright <wainwrightml@googlemail.com>
2020-03-06 10:41:01 +0000

    Started making tests for nuix

---
+++ NuixClientTests/IntegrationTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using NUnit.Framework;
+
+namespace NuixClientTests
+{
+    class IntegrationTests
+    {
+        [Test]
+        public async Task TestCreateCase()
+        {
+            const string directoryPath = "D:/Test/TestCase";
+
+            Directory.Delete(directoryPath, true);
+
+            var process = new NuixClient.processes.NuixCreateCase()
+            {
+                CaseName = "Case Name",
+                CasePath = directoryPath,
+                Investigator = "Mark",
+                Description = "Description"
+            };
+
+            await AssertNoErrors(process.Execute());
+
+            var lines =  process.Execute();
+            
+
+
+        }
+
+
+        private static async Task AssertNoErrors(IAsyncEnumerable<Result<string>> lines)
+        {
+            var errors = new List<string>();
+
+            await foreach (var (_, isFailure, _, error) in lines)
+            {
+                if (isFailure)
+                    errors.Add(error);
+            }
+            
+            CollectionAssert.IsEmpty(errors);
+        }
+    }
+}
