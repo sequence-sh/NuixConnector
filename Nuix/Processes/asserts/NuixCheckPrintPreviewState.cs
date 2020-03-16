656fa00ff2a8e314ece723f98bcf35f697a93675
Mark Wainwright <wainwrightml@googlemail.com>
2020-03-16 13:14:24 +0000

    Added check print preview state process

---
+++ Nuix/Processes/asserts/NuixCheckPrintPreviewState.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Reductech.EDR.Connectors.Nuix.enums;
+using Reductech.EDR.Utilities.Processes;
+using YamlDotNet.Serialization;
+
+namespace Reductech.EDR.Connectors.Nuix.processes.asserts
+{
+    /// <summary>
+    /// Checks the print preview state of the production set.
+    /// </summary>
+    public sealed class NuixCheckPrintPreviewState : RubyScriptAssertionProcess
+    {
+        /// <inheritdoc />
+        public override IEnumerable<string> GetArgumentErrors()
+        {
+            yield break;
+        }
+
+        /// <inheritdoc />
+        public override string GetName()
+        {
+            return $"Assert preview state is {ExpectedState}";
+        }
+
+        /// <inheritdoc />
+        internal override string ScriptName => "GetPrintPreviewState.rb";
+
+        /// <inheritdoc />
+        internal override IEnumerable<(string arg, string val)> GetArgumentValuePairs()
+        {
+            yield return ("-p", CasePath);
+            yield return ("-n", ProductionSetName);
+        }
+
+        /// <inheritdoc />
+        protected override (bool success, string? failureMessage)? InterpretLine(string s)
+        {
+            var pps = s.ToLowerInvariant() switch
+            {
+                "all" => PrintPreviewState.All,
+                "some" => PrintPreviewState.Some,
+                "none" => PrintPreviewState.None,
+                _ => null as PrintPreviewState?
+            };
+
+            if (pps == null) return null;
+
+            if (pps == ExpectedState)
+                return (true, null);
+
+            return (false, $"Expected print preview state '{ExpectedState}' but was '{pps.Value}'");
+        }
+
+        /// <inheritdoc />
+        protected override string DefaultFailureMessage => "Could not confirm print preview state";
+
+        /// <summary>
+        /// The expected print preview state of the production set;
+        /// </summary>
+        [YamlMember(Order = 2)] 
+        public PrintPreviewState ExpectedState { get; set; } = PrintPreviewState.All;
+
+        /// <summary>
+        /// The production set to reorder.
+        /// </summary>
+        [Required]
+        [YamlMember(Order = 3)]
+#pragma warning disable CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
+        public string ProductionSetName { get; set; }
+
+
+        /// <summary>
+        /// The path to the case.
+        /// </summary>
+        [Required]
+        [YamlMember(Order = 4)]
+        [ExampleValue("C:/Cases/MyCase")]
+        public string CasePath { get; set; }
+#pragma warning restore CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
+    }
+}
---
+++ Nuix/scripts/GetPrintPreviewState.rb
@@ -0,0 +1,47 @@
+require 'optparse'
+
+hash_options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: your_app [options]"
+  opts.on('-p [ARG]', '--path [ARG]', "Case Path") do |v|
+    hash_options[:pathArg] = v
+  end
+  opts.on('-n [ARG]', '--productionSetName [ARG]', "Production Set Name") do |v|
+    hash_options[:productionSetNameArg] = v
+  end
+  opts.on('--version', 'Display the version') do 
+    puts "VERSION"
+    exit
+  end
+  opts.on('-h', '--help', 'Display this help') do 
+    puts opts
+    exit
+  end
+
+end.parse!
+requiredArguments = [:pathArg, :productionSetNameArg] 
+
+unless requiredArguments.all? {|a| hash_options[a] != nil}
+    puts "Missing arguments #{(requiredArguments.select {|a| hash_options[a] == nil}).to_s}"
+
+else
+    puts "Opening Case"
+    
+    the_case = utilities.case_factory.open(hash_options[:pathArg])
+
+    productionSet = the_case.findProductionSetByName(hash_options[:productionSetNameArg])
+
+        if(productionSet == nil)        
+            puts "Production Set Not Found"
+        else            
+            puts "Production Set Found"
+
+            r = productionSet.getPrintPreviewState()
+
+            puts r
+        end 
+
+    the_case.close
+    puts "Case Closed"
+    
+end
