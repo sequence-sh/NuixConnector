688540b85c66cc2be3d043f48b6ad0ffc78db698
Mark Wainwright <wainwrightml@googlemail.com>
2020-03-19 09:20:19 +0000

    Created NuixStep

---
+++ Nuix/steps/NuixStep.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Superpower.Model;
+
+namespace Reductech.EDR.Connectors.Nuix.steps
+{
+    public class ComposedNuixProcess
+    {
+
+        public async IAsyncEnumerable<Result<string>> Execute(INuixProcessSettings nuixProcessSettings)
+        {
+            var allLines = Steps.SelectMany(x => x.Lines).ToList();
+        }
+
+        private IReadOnlyCollection<NuixStep> Steps { get; }
+    }
+
+
+
+    internal abstract class NuixStep
+    {
+        public abstract IEnumerable<string> Lines { get; }
+
+
+    }
+}
