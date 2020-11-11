20df9482a7f2183990f2f5f65dfa6d19dc0b3bdf
Mark Wainwright <wainwrightml@gmail.com>
2020-11-11 11:42:31 +0000

    Added more Converters for commonly used methods

---
+++ Nuix/Conversion/FromStreamConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Reductech.EDR.Connectors.Nuix.Steps.Meta;
+using Reductech.EDR.Core.Internal;
+using Reductech.EDR.Core.Steps;
+
+namespace Reductech.EDR.Connectors.Nuix.Conversion
+{
+    internal sealed class FromStreamConverter : CoreTypedMethodConverter<FromStream, string>
+    {
+        /// <inheritdoc />
+        protected override IEnumerable<(RubyFunctionParameter parameter, IStep argumentProcess)> GetArgumentBlocks(FromStream process)
+        {
+            yield return (StreamArgument, process.Stream);
+        }
+
+        /// <inheritdoc />
+        public override string FunctionName => "FromStream";
+
+        /// <inheritdoc />
+        public override string FunctionText { get; } = $"{StreamArgument}";
+
+        public static readonly RubyFunctionParameter StreamArgument = new RubyFunctionParameter("streamArg", nameof(FromStream.Stream), false, null);
+
+        /// <inheritdoc />
+        public override IReadOnlyCollection<RubyFunctionParameter> Arguments { get; } = new[] {StreamArgument};
+    }
+}
