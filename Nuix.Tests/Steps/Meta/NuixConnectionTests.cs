b8c05ff5ed3846f34abe36b38d3da7d2c86ae9dc
antonyoni <antony@reduc.tech>
2020-12-03 19:28:18 +0000

    Add tests for NuixConnectionHelper

---
+++ Nuix.Tests/Steps/Meta/NuixConnectionTests.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Reductech.EDR.Connectors.Nuix.Steps;
+using Reductech.EDR.Connectors.Nuix.Steps.Meta;
+using Reductech.EDR.Connectors.Nuix.Steps.Meta.ConnectionObjects;
+using Reductech.EDR.Core;
+using Reductech.EDR.Core.ExternalProcesses;
+using Reductech.EDR.Core.Internal;
+using Reductech.EDR.Core.TestHarness;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Reductech.EDR.Connectors.Nuix.Tests.Steps.Meta
+{
+    public class NuixConnectionHelperTests
+    {
+        private readonly NuixSettings _nuixSettings = new NuixSettings(true,
+            Constants.NuixSettingsList.First().NuixExeConsolePath,
+            new Version(8, 8),
+            Constants.AllNuixFeatures);
+
+        //private Result<NuixConnection, IErrorBuilder> GetConnection()
+        private static StateMonad GetStateMonad(IExternalProcessRunner externalProcessRunner)
+        {
+            var nuixSettings = new NuixSettings(
+                true,
+                Constants.NuixSettingsList.First().NuixExeConsolePath,
+                new Version(8, 8),
+                Constants.AllNuixFeatures
+            );
+            
+            var sfs = StepFactoryStore.CreateUsingReflection(typeof(IStep), typeof(IRubyScriptStep));
+
+            var monad = new StateMonad(
+                new TestLogger(), 
+                nuixSettings,
+                externalProcessRunner,
+                FileSystemHelper.Instance,
+                sfs
+            );
+
+            return monad;
+        }
+
+        private static IStateMonad GetStateMonadWithConnection()
+        {
+            var fakeExternalProcess = new ExternalProcessMock(2, GetCreateCaseAction());
+            
+            IStateMonad state = GetStateMonad(fakeExternalProcess);
+            
+            var nuixSettings = state.GetSettings<INuixSettings>();
+
+            fakeExternalProcess.ProcessPath = nuixSettings.Value.NuixExeConsolePath;
+
+            var process = state.ExternalProcessRunner.StartExternalProcess(
+                fakeExternalProcess.ProcessPath,
+                fakeExternalProcess.ProcessArgs,
+                fakeExternalProcess.ProcessEncoding
+            );
+            
+            if (process.IsFailure)
+                throw new XunitException("Failed to create a mock Nuix process");
+
+            var connection = new NuixConnection(process.Value);
+
+            var setResult = state.SetVariable(NuixConnectionHelper.NuixVariableName, connection);
+            
+            if (setResult.IsFailure)
+                throw new XunitException("Could not set existing connection on state monad.");
+
+            return state;
+        }
+
+        private static ExternalProcessAction GetCreateCaseAction()
+        {
+            return new ExternalProcessAction(new ConnectionCommand
+                {
+                    Command = "CreateCase",
+                    FunctionDefinition = "",
+                    Arguments = new Dictionary<string, object>
+                    {
+                        {nameof(NuixCreateCase.CasePath), "d:\\case"},
+                        {nameof(NuixCreateCase.CaseName), "Integration Test Case"},
+                        {nameof(NuixCreateCase.Investigator), "Mark"}
+                    }
+                },
+                new ConnectionOutput
+                {
+                    Result = new ConnectionOutputResult {Data = null}
+                }
+            );
+        }
+
+        [Fact]
+        public void GetOrCreateNuixConnection_WhenConnectionExists_ReturnsConnection()
+        {
+            var state = GetStateMonadWithConnection();
+            
+            var expected = state.GetVariable<NuixConnection>(NuixConnectionHelper.NuixVariableName);
+
+            var createConnection = state.GetOrCreateNuixConnection(false);
+
+            Assert.True(createConnection.IsSuccess);
+            Assert.Same(expected.Value, createConnection.Value);
+        }
+
+        [Fact]
+        public void GetOrCreateNuixConnection_WhenReopenIsSet_DisposesOldConnection()
+        {
+            var state = GetStateMonadWithConnection();
+            
+            var originalConnection = state.GetVariable<NuixConnection>(NuixConnectionHelper.NuixVariableName);
+
+            var createConnection = state.GetOrCreateNuixConnection(true);
+
+            var processRef = originalConnection.Value.ExternalProcess as ExternalProcessMock.ProcessReferenceMock;
+
+            Assert.True(createConnection.IsSuccess);
+            Assert.True(processRef!.IsDisposed);
+        }
+
+    }
+}
