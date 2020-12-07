7b1941e91e6c1d8aa56c18434edbf745db17c6b9
Mark Wainwright <wainwrightml@gmail.com>
2020-12-07 13:09:13 +0000

    Added Integration tests for NuixRunScriptTests

--- Nuix.Tests/Steps/NuixRunScriptTests.cs
+++ Nuix.Tests/Steps/NuixRunScriptTests.cs
@@ -1,14 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+using FluentAssertions;
 using Moq;
 using Reductech.EDR.Connectors.Nuix.Steps.Meta;
 using Reductech.EDR.Connectors.Nuix.Steps.Meta.ConnectionObjects;
 using Reductech.EDR.Core.Entities;
 using Reductech.EDR.Core.ExternalProcesses;
 using Reductech.EDR.Core.Internal;
+using Reductech.EDR.Core.Internal.Errors;
+using Reductech.EDR.Core.Serialization;
 using Reductech.EDR.Core.TestHarness;
+using Reductech.EDR.Core.Util;
+using Reductech.Utilities.Testing;
+using Xunit;
 using Xunit.Abstractions;
+using Entity = Reductech.EDR.Core.Entities.Entity;
 
 namespace Reductech.EDR.Connectors.Nuix.Tests.Steps
 {
@@ -115,6 +125,49 @@ EntityStreamParameter:
             }
         }
 
+        [Fact]
+
+        [Trait("Category", "Integration")]
+        public async Task TestScriptWithNoStream_Integration()
+        {
+            var stepCase = new IntegrationTestCase("Case with no stream",
+                new NuixRunScript
+                {
+                    FunctionName = Constant("Test Script"),
+                    ScriptText = Constant("puts Param1\r\nreturn Param2"),
+                    EntityStreamParameter = new Constant<EntityStream>(new EntityStream(new List<Entity>()
+                        {
+                            CreateEntity(("Foo", "a")),
+                            CreateEntity(("Foo", "b")),
+                        }.ToAsyncEnumerable())),
+                    Parameters = new Constant<Entity>(CreateEntity(("Param1", "ABC"), ("Param2", "DEF")))
+
+                },
+                "DEF",
+                "ABC").WithSettings(Constants.NuixSettingsList.OrderByDescending(x=>x.NuixVersion).First());
+
+            await stepCase.RunCaseAsync(TestOutputHelper, null);
+        }
+
+        [Fact]
+        [Trait("Category", "Integration")]
+        public async Task TestScriptWithStream_Integration()
+        {
+            var stepCase = new IntegrationTestCase("Case with stream",
+                new NuixRunScript
+                {
+                    FunctionName = Constant("Test Script"),
+                    ScriptText = Constant("puts Param1\r\nreturn Param2"),
+                    EntityStreamParameter = null,
+                    Parameters = new Constant<Entity>(CreateEntity(("Param1", "ABC"), ("Param2", "DEF")))
+
+                },
+                "DEF",
+                "ABC").WithSettings(Constants.NuixSettingsList.OrderByDescending(x=>x.NuixVersion).First());;
+
+            await stepCase.RunCaseAsync(TestOutputHelper, null);
+        }
+
 
         public class RunScriptStepCase : StepCase
         {
@@ -135,6 +188,60 @@ EntityStreamParameter:
             /// <inheritdoc />
             public override IExternalProcessRunner GetExternalProcessRunner(MockRepository mockRepository) =>
                 new ExternalProcessMock(1, ExternalProcessActions.ToArray());
+        }
+
+        public class IntegrationTestCase : CaseThatExecutes
+        {
+            public IntegrationTestCase(string name, NuixRunScript step, string expectedOutput, params string[] expectedLoggedValues)
+                : base(expectedLoggedValues)
+            {
+                Name = name;
+                Step = step;
+                IgnoreFinalState = true;
+                ExpectedOutput = expectedOutput;
+            }
+
+            public override string Name { get; }
+
+            public NuixRunScript Step { get; }
+
+            public string ExpectedOutput { get; }
+
+            /// <inheritdoc />
+            public override async Task<IStep> GetStepAsync(ITestOutputHelper testOutputHelper, string? extraArgument)
+            {
+                var yaml = await Step.Unfreeze().SerializeToYamlAsync(CancellationToken.None);
+
+                var deserializedStep = YamlMethods.DeserializeFromYaml(yaml, StepFactoryStore.CreateUsingReflection(typeof(IStep), typeof(NuixRunScript)));
+
+                deserializedStep.ShouldBeSuccessful(x => x.AsString);
+
+                var unfrozenStep = deserializedStep.Value.TryFreeze();
+
+                unfrozenStep.ShouldBeSuccessful(x => x.AsString);
+
+                return unfrozenStep.Value;
+            }
+
+            /// <inheritdoc />
+            public override void CheckUnitResult(Result<Unit, IError> result)
+            {
+                result.ShouldBeSuccessful(x => x.AsString);
+            }
+
+            /// <inheritdoc />
+            public override void CheckOutputResult(Result<string, IError> result)
+            {
+                result.ShouldBeSuccessful(x => x.AsString);
+
+                result.Value.Should().Be(ExpectedOutput);
+            }
+
+            /// <inheritdoc />
+            public override IFileSystemHelper GetFileSystemHelper(MockRepository mockRepository) => FileSystemHelper.Instance;
+
+            /// <inheritdoc />
+            public override IExternalProcessRunner GetExternalProcessRunner(MockRepository mockRepository) => ExternalProcessRunner.Instance;
         }
 
 
