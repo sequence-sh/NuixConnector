c59f1f6566670c2f5237b06166fd11798250b1e2
antonyoni <antony@reduc.tech>
2020-12-08 21:52:59 +0000

    Add FakeNuixStreamFunction

--- Nuix.Tests/Steps/Meta/FakeNuixFunction.cs
+++ Nuix.Tests/Steps/Meta/FakeNuixFunction.cs
@@ -9,6 +9,26 @@ using Reductech.EDR.Core.Util;
 
 namespace Reductech.EDR.Connectors.Nuix.Tests.Steps.Meta
 {
+    public class FakeNuixStreamFunctionFactory : RubyScriptStepFactory<FakeNuixStreamFunction, string>
+    {
+        internal FakeNuixStreamFunctionFactory() { }
+        public static RubyScriptStepFactory<FakeNuixStreamFunction, string> Instance { get; } = new FakeNuixStreamFunctionFactory();
+        public override Version RequiredNuixVersion { get; } = new Version(8, 0);
+        public override IReadOnlyCollection<NuixFeature> RequiredFeatures { get; } = new List<NuixFeature>();
+        public override string FunctionName => "EntityStream";
+        public override string RubyFunctionText => @"log messageArg";
+    }
+
+    public class FakeNuixStreamFunction : RubyScriptStepBase<string>
+    {
+        public override IRubyScriptStepFactory<string> RubyScriptStepFactory => FakeNuixStreamFunctionFactory.Instance;
+
+        [Required]
+        [StepProperty]
+        [RubyArgument("entityStream", 1)]
+        public IStep<EntityStream> EntityStream { get; set; } = null!;
+    }
+
     public sealed class FakeNuixTwoStreamFunctionFactory : RubyScriptStepFactory<FakeNuixTwoStreamFunction, Unit>
     {
         internal FakeNuixTwoStreamFunctionFactory() { }
@@ -22,15 +42,15 @@ namespace Reductech.EDR.Connectors.Nuix.Tests.Steps.Meta
     public class FakeNuixTwoStreamFunction : RubyScriptStepBase<Unit>
     {
         public override IRubyScriptStepFactory<Unit> RubyScriptStepFactory => FakeNuixTwoStreamFunctionFactory.Instance;
-        
+
         [Required]
         [StepProperty]
-        [RubyArgument("stream1Arg", 2)]
+        [RubyArgument("stream1Arg", 1)]
         public IStep<EntityStream> Stream1 { get; set; } = null!;
-        
+
         [Required]
         [StepProperty]
-        [RubyArgument("stream2Arg", 3)]
+        [RubyArgument("stream2Arg", 2)]
         public IStep<EntityStream> Stream2 { get; set; } = null!;
     }
 
