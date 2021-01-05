f7d3ee0a8c0536adb29473044a6cefa5bb807141
antonyoni <antony@reduc.tech>
2021-01-05 21:25:31 +0000

    Formatting

--- Nuix/Enums/ItemSetDeduplication.cs
+++ Nuix/Enums/ItemSetDeduplication.cs
@@ -11,26 +11,31 @@ namespace Reductech.EDR.Connectors.Nuix.Enums
         /// MD5RankedCustodian if a custodian ranking is given, MD5 otherwise
         /// </summary>
         Default,
+
         /// <summary>
         /// MD5 results in items with the same MD5 hash being identical. Tie breaking is by highest path order.
         /// </summary>
         [Description("MD5")]
         MD5,
+
         /// <summary>
         /// MD5 Per Custodian results in items with the same MD5 hash and custodian being identical. Tie breaking is by highest path order.
         /// </summary>
         [Description("MD5 Per Custodian")]
         MD5PerCustodian,
+
         /// <summary>
         /// MD5 Ranked Custodian results in items with MD5 hash being identical. Tie breaking is by the item with the highest ranked custodian or highest path order if custodian ranking is equal.
         /// </summary>
         [Description("MD5 Ranked Custodian")]
         MD5RankedCustodian,
+
         /// <summary>
         /// Scripted results in items being deduplicated based on an expression defined by the script and passed to ItemSet.addItems. It is an error to add items to this Item Set without supplying the expression.
         /// </summary>
         [Description("Scripted")]
         Scripted,
+
         /// <summary>
         /// None results in all items being added to the set without deduplication.
         /// </summary>
