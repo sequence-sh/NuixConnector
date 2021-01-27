f1fec7817c7969d783b6e1125bccb3b1f4b5d54a
Mark Wainwright <wainwrightml@gmail.com>
2021-01-27 13:35:16 +0000

    Added extra extension methods to nuix settings

--- Nuix/NuixSettings.cs
+++ Nuix/NuixSettings.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using CSharpFunctionalExtensions;
 using Reductech.EDR.Connectors.Nuix.Steps.Meta;
 using Reductech.EDR.Core;
 using Entity = Reductech.EDR.Core.Entity;
@@ -56,6 +58,74 @@ public static class NuixSettings
     /// </summary>
     public static readonly IReadOnlyList<string> DongleArguments =
         new List<string> { "-licencesourcetype", "dongle" };
+
+    /// <summary>
+    /// Tries to get a nested string.
+    /// </summary>
+    public static Maybe<string>
+        TryGetNestedString(
+            this Entity current,
+            params string[] properties) //TODO remove when we can update core
+    {
+        if (!properties.Any())
+            return Maybe<string>.None;
+
+        foreach (var property in properties.SkipLast(1))
+        {
+            var v = current.TryGetValue(property);
+
+            if (v.HasNoValue)
+                return Maybe<string>.None;
+
+            if (v.Value.TryPickT7(out var e, out _))
+                current = e;
+            else
+                return Maybe<string>.None;
+        }
+
+        var lastProp = current.TryGetValue(properties.Last());
+
+        if (lastProp.HasNoValue)
+            return Maybe<string>.None;
+
+        return lastProp.Value.ToString();
+    }
+
+    /// <summary>
+    /// Tries to get a nested string.
+    /// </summary>
+    public static Maybe<string[]>
+        TryGetNestedList(
+            this Entity current,
+            params string[] properties) //TODO remove when we can update core
+    {
+        if (!properties.Any())
+            return Maybe<string[]>.None;
+
+        foreach (var property in properties.SkipLast(1))
+        {
+            var v = current.TryGetValue(property);
+
+            if (v.HasNoValue)
+                return Maybe<string[]>.None;
+
+            if (v.Value.TryPickT7(out var e, out _))
+                current = e;
+            else
+                return Maybe<string[]>.None;
+        }
+
+        var lastProp = current.TryGetValue(properties.Last());
+
+        if (lastProp.HasNoValue)
+            return Maybe<string[]>.None;
+
+        if (!lastProp.Value.TryPickT8(out var list, out _))
+            return Maybe<string[]>.None;
+
+        var stringArray = list.Select(x => x.ToString()).ToArray();
+        return stringArray;
+    }
 }
 
 }
