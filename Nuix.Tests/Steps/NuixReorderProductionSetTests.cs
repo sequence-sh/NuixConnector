0140b7b11edd8f21168d7c99cb2c6fa9ec7e9da4
Mark Wainwright <Mark@reduc.tech>
2021-02-24 12:59:08 +0000

    Changed nuix connection json converted to serialize enums according to Display Attribute

--- Nuix.Tests/Steps/NuixReorderProductionSetTests.cs
+++ Nuix.Tests/Steps/NuixReorderProductionSetTests.cs
@@ -1,18 +1,46 @@
 ﻿using System.Collections.Generic;
+using Reductech.EDR.Connectors.Nuix.Enums;
 using Reductech.EDR.Connectors.Nuix.Steps;
 using Reductech.EDR.Core.Util;
+using static Reductech.EDR.Connectors.Nuix.Tests.Constants;
+using static Reductech.EDR.Core.TestHarness.StaticHelpers;
 
 namespace Reductech.EDR.Connectors.Nuix.Tests.Steps
 {
 
-public  partial class NuixReorderProductionSetTests : NuixStepTestBase<NuixReorderProductionSet, Unit>
+public partial class
+    NuixReorderProductionSetTests : NuixStepTestBase<NuixReorderProductionSet, Unit>
 {
-
     /// <inheritdoc />
     protected override IEnumerable<DeserializeCase> DeserializeCases { get { yield break; } }
 
     /// <inheritdoc />
-    protected override IEnumerable<NuixIntegrationTestCase> NuixTestCases { get { yield break; } }
+    protected override IEnumerable<NuixIntegrationTestCase> NuixTestCases
+    {
+        get
+        {
+            yield return new NuixIntegrationTestCase(
+                "Reorder From Production Set",
+                DeleteCaseFolder,
+                CreateCase,
+                AddData,
+                new NuixAddToProductionSet
+                {
+                    SearchTerm            = Constant("*.txt"),
+                    ProductionSetName     = Constant("fullset"),
+                    ProductionProfilePath = TestProductionProfilePath
+                },
+                new NuixReorderProductionSet()
+                {
+                    ProductionSetName = Constant("fullset"),
+                    SortOrder         = Constant(ProductionSetSortOrder.TopLevelItemDate)
+                },
+                AssertCount(2, "production-set:fullset"),
+                new NuixCloseConnection(),
+                DeleteCaseFolder
+            );
+        }
+    }
 }
 
 }
--- Nuix/Enums/ProductionSetSortOrder.cs
+++ Nuix/Enums/ProductionSetSortOrder.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Reductech.EDR.Connectors.Nuix.Enums
 {
@@ -12,24 +13,28 @@ public enum ProductionSetSortOrder
     /// Default sort order (fastest). Sorts as documented in ItemSorter.sortItemsByPosition(List).
     /// </summary>
     [Description("position")]
+    [Display(Name = "position")]
     Position,
 
     /// <summary>
     /// Sorts as documented in ItemSorter.sortItemsByTopLevelItemDate(List).
     /// </summary>
     [Description("top_level_item_date")]
+    [Display(Name = "top_level_item_date")]
     TopLevelItemDate,
 
     /// <summary>
     /// Sorts as documented in ItemSorter.sortItemsByTopLevelItemDateDescending(List).
     /// </summary>
     [Description("top_level_item_date_descending")]
+    [Display(Name = "top_level_item_date_descending")]
     TopLevelItemDateDescending,
 
     /// <summary>
     /// Sorts items based on their document IDs for the production set.
     /// </summary>
     [Description("document_id")]
+    [Display(Name = "document_id")]
     DocumentId
 }
 
