4811393370c7e2605d70791dd965b5bdda55623d
antonyoni <antony@reduc.tech>
2021-03-08 21:26:17 +0000

    Add NuixSearch helper function

---
+++ Nuix/Steps/Helpers/NuixSearch.cs
@@ -0,0 +1,41 @@
+﻿using Reductech.EDR.Connectors.Nuix.Steps.Meta;
+
+namespace Reductech.EDR.Connectors.Nuix.Steps.Helpers
+{
+
+/// <summary>
+/// Search a case and return a collection of items.
+/// </summary>
+public class NuixSearch : IRubyHelper
+{
+    /// <inheritdoc />
+    public string FunctionName { get; } = "Search";
+
+    /// <inheritdoc />
+    public string FunctionText { get; } = @"
+def search(search_term, search_options, sort)
+  log ""Searching for '#{search_term}'""
+
+  opts = search_options.nil? ? {} : search_options
+  log(""Search options: #{opts}"", severity: :trace)
+
+  if sort.nil? || !sort
+    log('Search results will be unsorted', severity: :trace)
+    items = $current_case.search_unsorted(search_term, opts)
+  else
+    log('Search results will be sorted', severity: :trace)
+    items = $current_case.search(search_term, opts)
+  end
+
+  log ""Items found: #{items.length}""
+  return items
+end
+";
+
+    /// <summary>
+    /// An instance of the helper function
+    /// </summary>
+    public static IRubyHelper Instance => new NuixSearch();
+}
+
+}
