65f6a9b5ce22fcd3178ee971907020b99853b06d
antonyoni <antony@reduc.tech>
2021-03-09 12:45:38 +0000

    Add localised logging

---
+++ Nuix/Logging/LogSituationNuix.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+using Reductech.EDR.Core.Internal.Logging;
+
+namespace Reductech.EDR.Connectors.Nuix.Logging
+{
+
+/// <summary>
+/// Identifying code for a Core log situation.
+/// </summary>
+public sealed record LogSituationNuix : LogSituationBase
+{
+    private LogSituationNuix(string code, LogLevel logLevel) : base(code, logLevel) { }
+
+    /// <inheritdoc />
+    protected override string GetLocalizedString()
+    {
+        var localizedMessage = LogMessages_EN.ResourceManager.GetString(Code);
+
+        Debug.Assert(localizedMessage != null, nameof(localizedMessage) + " != null");
+        return localizedMessage;
+    }
+
+    /// <summary>
+    /// Helper function {FunctionName} already exists.
+    /// </summary>
+    public static readonly LogSituationNuix HelperExists = new(
+        nameof(HelperExists),
+        LogLevel.Trace
+    );
+}
+
+}
