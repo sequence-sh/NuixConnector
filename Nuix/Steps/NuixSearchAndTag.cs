0fe1c1a8701b8791817d074d0ecec6238b097b33
antonyoni <antony@reduc.tech>
2021-03-11 14:26:24 +0000

    Refactor SearchAndTag to use the new RubySearchStepBase class

--- Nuix/Steps/NuixSearchAndTag.cs
+++ Nuix/Steps/NuixSearchAndTag.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using Reductech.EDR.Connectors.Nuix.Enums;
-using Reductech.EDR.Connectors.Nuix.Steps.Helpers;
 using Reductech.EDR.Connectors.Nuix.Steps.Meta;
 using Reductech.EDR.Core;
 using Reductech.EDR.Core.Attributes;
@@ -15,7 +13,7 @@ namespace Reductech.EDR.Connectors.Nuix.Steps
 /// <summary>
 /// Searches a NUIX case with a particular search string and tags all files it finds.
 /// </summary>
-public sealed class NuixSearchAndTagStepFactory : RubyScriptStepFactory<NuixSearchAndTag, Unit>
+public sealed class NuixSearchAndTagStepFactory : RubySearchStepFactory<NuixSearchAndTag, Unit>
 {
     private NuixSearchAndTagStepFactory() { }
 
@@ -31,10 +29,6 @@ public sealed class NuixSearchAndTagStepFactory : RubyScriptStepFactory<NuixSear
     /// <inheritdoc />
     public override IReadOnlyCollection<NuixFeature> RequiredFeatures { get; }
         = new List<NuixFeature> { NuixFeature.ANALYSIS };
-
-    /// <inheritdoc />
-    public override IReadOnlyCollection<IRubyHelper> RequiredHelpers { get; }
-        = new List<IRubyHelper> { NuixSearch.Instance, NuixExpandSearch.Instance };
 
     /// <inheritdoc />
     public override string FunctionName => "SearchAndTag";
@@ -56,21 +50,11 @@ public sealed class NuixSearchAndTagStepFactory : RubyScriptStepFactory<NuixSear
 /// <summary>
 /// Searches a NUIX case with a particular search string and tags all files it finds.
 /// </summary>
-public sealed class NuixSearchAndTag : RubyCaseScriptStepBase<Unit>
+public sealed class NuixSearchAndTag : RubySearchStepBase<Unit>
 {
     /// <inheritdoc />
     public override IRubyScriptStepFactory<Unit> RubyScriptStepFactory =>
         NuixSearchAndTagStepFactory.Instance;
-
-    /// <summary>
-    /// The term to search for.
-    /// </summary>
-    [Required]
-    [StepProperty(1)]
-    [Example("*.txt")]
-    [RubyArgument("searchArg")]
-    [Alias("Search")]
-    public IStep<StringStream> SearchTerm { get; set; } = null!;
 
     /// <summary>
     /// The tag to assign to found results.
@@ -79,39 +63,6 @@ public sealed class NuixSearchAndTag : RubyCaseScriptStepBase<Unit>
     [StepProperty(2)]
     [RubyArgument("tagArg")]
     public IStep<StringStream> Tag { get; set; } = null!;
-
-    /// <summary>
-    /// Pass additional search options to nuix. For an unsorted search (default)
-    /// the only available option is defaultFields. When using <code>SortSearch=true</code>
-    /// the options are defaultFields, order, and limit.
-    /// Please see the nuix API for <code>Case.search</code>
-    /// and <code>Case.searchUnsorted</code> for more details.
-    /// </summary>
-    [StepProperty(3)]
-    [RubyArgument("searchOptionsArg")]
-    [DefaultValueExplanation("No search options provided")]
-    public IStep<Entity>? SearchOptions { get; set; }
-
-    /// <summary>
-    /// By default the search is not sorted by relevance which
-    /// increases performance. Set this to true to sort the
-    /// search by relevance.
-    /// </summary>
-    [StepProperty(4)]
-    [RubyArgument("sortArg")]
-    [DefaultValueExplanation("false")]
-    public IStep<bool>? SortSearch { get; set; }
-
-    /// <summary>
-    /// Defines the type of search that is done. By default only the items
-    /// responsive to the search terms are tagged, but the result set
-    /// can be augmented using this parameter.
-    /// </summary>
-    [StepProperty(5)]
-    [RubyArgument("searchTypeArg")]
-    [DefaultValueExplanation("ItemsOnly")]
-    public IStep<SearchType> SearchType { get; set; } =
-        new EnumConstant<SearchType>(Enums.SearchType.ItemsOnly);
 }
 
 }
