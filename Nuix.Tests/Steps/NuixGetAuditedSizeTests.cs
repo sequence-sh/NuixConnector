14efb63aa700c7c76f9c62ab2f97cf36d9fb1c0b
antonyoni <antony@reduc.tech>
2021-03-12 11:14:23 +0000

    Added GetAuditedSize step and tests Closes #169

---
+++ Nuix.Tests/Steps/NuixGetAuditedSizeTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Reductech.EDR.Connectors.Nuix.Steps;
+using Reductech.EDR.Core.Internal;
+using Reductech.EDR.Core.Steps;
+using static Reductech.EDR.Core.TestHarness.StaticHelpers;
+using static Reductech.EDR.Connectors.Nuix.Tests.Constants;
+
+namespace Reductech.EDR.Connectors.Nuix.Tests.Steps
+{
+
+public partial class NuixGetAuditedSizeTests : NuixStepTestBase<NuixGetAuditedSize, double>
+{
+    /// <inheritdoc />
+    protected override IEnumerable<NuixIntegrationTestCase> NuixTestCases
+    {
+        get
+        {
+            yield return new NuixIntegrationTestCase(
+                "Ingest items and calculate audited size",
+                DeleteCaseFolder,
+                CreateCase,
+                new NuixAddItem
+                {
+                    Custodian  = Constant("Mark"),
+                    Paths      = DataPaths,
+                    FolderName = Constant("New Folder"),
+                    ProcessingSettings = Constant(
+                        Core.Entity.Create(("calculateAuditedSize", true))
+                    )
+                },
+                new AssertTrue
+                {
+                    Boolean = new Equals<double>()
+                    {
+                        Terms = new ArrayNew<double>()
+                        {
+                            Elements = new List<IStep<double>>()
+                            {
+                                Constant(2.5), new NuixGetAuditedSize()
+                            }
+                        }
+                    }
+                },
+                new NuixCloseConnection(),
+                DeleteCaseFolder
+            );
+        }
+    }
+}
+
+}
---
+++ Nuix/Steps/NuixGetAuditedSize.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using Reductech.EDR.Connectors.Nuix.Steps.Meta;
+using Reductech.EDR.Core;
+using Reductech.EDR.Core.Attributes;
+using Reductech.EDR.Core.Internal;
+
+namespace Reductech.EDR.Connectors.Nuix.Steps
+{
+
+/// <summary>
+/// Calculate the total audited size for a case.
+/// For this to work, the case material needs to have been ingested
+/// with the calculateAuditedSize=true processing setting.
+/// </summary>
+public sealed class
+    NuixGetAuditedSizeStepFactory : RubyScriptStepFactory<NuixGetAuditedSize, double>
+{
+    private NuixGetAuditedSizeStepFactory() { }
+
+    /// <summary>
+    /// The instance.
+    /// </summary>
+    public static RubyScriptStepFactory<NuixGetAuditedSize, double> Instance { get; } =
+        new NuixGetAuditedSizeStepFactory();
+
+    /// <inheritdoc />
+    public override Version RequiredNuixVersion { get; } = new(4, 2);
+
+    /// <inheritdoc />
+    public override IReadOnlyCollection<NuixFeature> RequiredFeatures { get; }
+        = new List<NuixFeature> { NuixFeature.ANALYSIS };
+
+    /// <inheritdoc />
+    public override string FunctionName => "GetAuditedSize";
+
+    /// <inheritdoc />
+    public override string RubyFunctionText => @"
+    items = $current_case.search_unsorted(searchArg)
+    log ""Calculating audited size for #{items.length} items""
+    size = 0
+    items.each {|i| size += i.get_audited_size }
+    log ""Done. Total size: #{size}""
+    return size
+";
+}
+
+/// <summary>
+/// Calculate the total audited size for a case.
+/// For this to work, the case material needs to have been ingested
+/// with the calculateAuditedSize=true processing setting.
+/// </summary>
+[Alias("NuixCalculateAuditedSize")]
+public sealed class NuixGetAuditedSize : RubyCaseScriptStepBase<double>
+{
+    /// <inheritdoc />
+    public override IRubyScriptStepFactory<double> RubyScriptStepFactory =>
+        NuixGetAuditedSizeStepFactory.Instance;
+
+    /// <summary>
+    /// The search term to use for calculating audited size.
+    /// This should be left as the default.
+    /// </summary>
+    [StepProperty(1)]
+    [RubyArgument("searchArg")]
+    [DefaultValueExplanation("flag:audited")]
+    [Alias("Search")]
+    public IStep<StringStream> SearchTerm { get; set; } = new StringConstant("flag:audited");
+}
+
+}
