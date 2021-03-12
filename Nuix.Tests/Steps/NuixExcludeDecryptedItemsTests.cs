a16cb4741100f54a83129a0ba81831d933743ab8
antonyoni <antony@reduc.tech>
2021-03-12 12:40:34 +0000

    Add NuixExcludeDecryptedItems step and tests Closes #170

---
+++ Nuix.Tests/Steps/NuixExcludeDecryptedItemsTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Reductech.EDR.Connectors.Nuix.Steps;
+using Reductech.EDR.Core.Internal;
+using Reductech.EDR.Core.Steps;
+using Reductech.EDR.Core.Util;
+using static Reductech.EDR.Core.TestHarness.StaticHelpers;
+using static Reductech.EDR.Connectors.Nuix.Tests.Constants;
+
+namespace Reductech.EDR.Connectors.Nuix.Tests.Steps
+{
+
+public partial class
+    NuixExcludeDecryptedItemsTests : NuixStepTestBase<NuixExcludeDecryptedItems, Unit>
+{
+    /// <inheritdoc />
+    protected override IEnumerable<NuixIntegrationTestCase> NuixTestCases
+    {
+        get
+        {
+            yield return new NuixIntegrationTestCase(
+                "Exclude decrypted item",
+                DeleteCaseFolder,
+                AssertCaseDoesNotExist,
+                CreateCase,
+                new NuixAddItem
+                {
+                    Custodian = Constant("Mark"),
+                    Paths = Array(
+                        Path.Combine(
+                            Directory.GetCurrentDirectory(),
+                            "AllData",
+                            "encrypted.pdf"
+                        )
+                    ),
+                    Container        = Constant("EncryptedItems"),
+                    PasswordFilePath = (PasswordFilePath)
+                },
+                AssertCount(2, "has-exclusion:0 AND \"encrypted\""),
+                new NuixExcludeDecryptedItems(),
+                AssertCount(1, "has-exclusion:0 AND \"encrypted\""),
+                new NuixCloseConnection(),
+                DeleteCaseFolder
+            );
+        }
+    }
+}
+
+}
---
+++ Nuix/Steps/NuixExcludeDecryptedItems.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using Reductech.EDR.Connectors.Nuix.Steps.Meta;
+using Reductech.EDR.Core;
+using Reductech.EDR.Core.Attributes;
+using Reductech.EDR.Core.Internal;
+using Reductech.EDR.Core.Util;
+
+namespace Reductech.EDR.Connectors.Nuix.Steps
+{
+
+/// <summary>
+/// Excludes password protected items once they have been decrypted.
+/// This only excludes encrypted original items that have a decrypted
+/// child with the same name.
+/// </summary>
+public sealed class
+    NuixExcludeDecryptedItemsStepFactory : RubyScriptStepFactory<NuixExcludeDecryptedItems, Unit>
+{
+    private NuixExcludeDecryptedItemsStepFactory() { }
+
+    /// <summary>
+    /// The instance.
+    /// </summary>
+    public static RubyScriptStepFactory<NuixExcludeDecryptedItems, Unit> Instance { get; } =
+        new NuixExcludeDecryptedItemsStepFactory();
+
+    /// <inheritdoc />
+    public override Version RequiredNuixVersion { get; } = new(4, 2);
+
+    /// <inheritdoc />
+    public override IReadOnlyCollection<NuixFeature> RequiredFeatures { get; }
+        = new List<NuixFeature> { NuixFeature.ANALYSIS };
+
+    /// <inheritdoc />
+    public override string FunctionName => "GetAuditedSize";
+
+    /// <inheritdoc />
+    public override string RubyFunctionText => @"
+    items = $current_case.search_unsorted(searchArg)
+    log ""Encrypted items found: #{items.length}""
+    return unless items.length > 0
+    excluded = 0
+    items.each do |item|
+      children = item.get_children
+      if children.count > 0
+        if item.name == children[0].name
+          excluded += 1
+          item.exclude(exclusionArg)
+          log(""Excluding #{item.name}"", severity: :trace)
+        end
+      end
+    end
+    if excluded > 0
+      log ""Excluded #{excluded} items with reason '#{exclusionArg}'""
+    else
+      log ""No items found to exclude""
+    end
+";
+}
+
+/// <summary>
+/// Excludes password protected items once they have been decrypted.
+/// This only excludes encrypted original items that have a decrypted
+/// child with the same name.
+/// </summary>
+public sealed class NuixExcludeDecryptedItems : RubyCaseScriptStepBase<Unit>
+{
+    /// <inheritdoc />
+    public override IRubyScriptStepFactory<Unit> RubyScriptStepFactory =>
+        NuixExcludeDecryptedItemsStepFactory.Instance;
+
+    /// <summary>
+    /// The search term used to find decrypted items.
+    /// </summary>
+    [StepProperty(1)]
+    [RubyArgument("searchArg")]
+    [DefaultValueExplanation("has-exclusion:0 AND flag:encrypted")]
+    [Alias("Search")]
+    public IStep<StringStream> SearchTerm { get; set; } =
+        new StringConstant("has-exclusion:0 AND flag:encrypted");
+
+    /// <summary>
+    /// The exclusion reason.
+    /// </summary>
+    [StepProperty(2)]
+    [RubyArgument("exclusionArg")]
+    [DefaultValueExplanation("DecryptedItem")]
+    [Alias("Exclusion")]
+    public IStep<StringStream> ExclusionReason { get; set; } = new StringConstant("DecryptedItem");
+}
+
+}
