5554b6192ac56e1c45789f63e4477958f05b326d
antonyoni <antony@reduc.tech>
2021-03-25 21:45:30 +0000

    Refactor tests using AssertPropertyValueEquals

--- Nuix.Tests/Steps/NuixGetLicenseDetailsTests.cs
+++ Nuix.Tests/Steps/NuixGetLicenseDetailsTests.cs
@@ -24,77 +24,10 @@ public partial class NuixGetLicenseDetailsTests : NuixStepTestBase<NuixGetLicens
                 {
                     Variable = VariableName.Entity, Value = new NuixGetLicenseDetails()
                 },
-                new AssertTrue
-                {
-                    Boolean = new Equals<StringStream>
-                    {
-                        Terms = new ArrayNew<StringStream>
-                        {
-                            Elements = new List<IStep<StringStream>>
-                            {
-                                Constant("enterprise-workstation"),
-                                new EntityGetValue<StringStream>
-                                {
-                                    Entity = new GetVariable<Entity>
-                                    {
-                                        Variable = VariableName.Entity
-                                    },
-                                    Property = Constant("Name")
-                                }
-                            }
-                        }
-                    }
-                },
-                new AssertTrue
-                {
-                    Boolean = new Equals<int>
-                    {
-                        Terms = new ArrayNew<int>
-                        {
-                            Elements = new List<IStep<int>>
-                            {
-                                Constant(2),
-                                new EntityGetValue<int>
-                                {
-                                    Entity = new GetVariable<Entity>
-                                    {
-                                        Variable = VariableName.Entity
-                                    },
-                                    Property = Constant("Workers")
-                                }
-                            }
-                        }
-                    }
-                },
-                new AssertTrue
-                {
-                    Boolean = new Equals<double>
-                    {
-                        Terms = new ArrayNew<double>
-                        {
-                            Elements = new List<IStep<double>>
-                            {
-                                Constant(5000000000.0),
-                                new EntityGetValue<double>
-                                {
-                                    Entity = new GetVariable<Entity>
-                                    {
-                                        Variable = VariableName.Entity
-                                    },
-                                    Property = Constant("AuditThreshold")
-                                }
-                            }
-                        }
-                    }
-                },
-                new AssertTrue
-                {
-                    Boolean = new EntityGetValue<bool>
-                    {
-                        Entity   = new GetVariable<Entity> { Variable = VariableName.Entity },
-                        Property = Constant("IsValid")
-                    }
-                },
+                AssertPropertyValueEquals("Name",           Constant("enterprise-workstation")),
+                AssertPropertyValueEquals("Workers",        Constant(2)),
+                AssertPropertyValueEquals("AuditThreshold", Constant(5000000000.0)),
+                AssertPropertyValueEquals("IsValid",        Constant(true)),
                 new NuixCloseConnection(),
                 DeleteCaseFolder
             );
