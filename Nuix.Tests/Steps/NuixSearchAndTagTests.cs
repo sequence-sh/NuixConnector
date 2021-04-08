f713589f5d51d6f379ce2fc565b5be70d58f1d3f
antonyoni <antony@reduc.tech>
2021-04-08 15:59:53 +0100

    Cleanup usings

--- Nuix.Tests/Steps/NuixSearchAndTagTests.cs
+++ Nuix.Tests/Steps/NuixSearchAndTagTests.cs
@@ -3,7 +3,6 @@ using System.IO;
 using Reductech.EDR.Connectors.Nuix.Enums;
 using Reductech.EDR.Connectors.Nuix.Steps;
 using Reductech.EDR.Core;
-using Reductech.EDR.Core.Internal;
 using Reductech.EDR.Core.Util;
 using static Reductech.EDR.Connectors.Nuix.Tests.Constants;
 using static Reductech.EDR.Core.TestHarness.StaticHelpers;
