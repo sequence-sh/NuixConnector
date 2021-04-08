73940f20acbdbb4bf7d4027aea690d9134513c85
antonyoni <antony@reduc.tech>
2021-04-08 19:07:32 +0100

    Update AddConcordance tests to use new parameters Remove tests step and deserialization tests, these are automated now

--- Nuix.Tests/Steps/NuixAddConcordanceTests.cs
+++ Nuix.Tests/Steps/NuixAddConcordanceTests.cs
@@ -1,9 +1,6 @@
 ﻿using System.Collections.Generic;
-using Moq;
 using Reductech.EDR.Connectors.Nuix.Steps;
-using Reductech.EDR.Connectors.Nuix.Steps.Meta.ConnectionObjects;
 using Reductech.EDR.Core;
-using Reductech.EDR.Core.TestHarness;
 using Reductech.EDR.Core.Util;
 using static Reductech.EDR.Connectors.Nuix.Tests.Constants;
 using static Reductech.EDR.Core.TestHarness.StaticHelpers;
@@ -13,110 +10,6 @@ namespace Reductech.EDR.Connectors.Nuix.Tests.Steps
 
 public partial class NuixAddConcordanceTests : NuixStepTestBase<NuixAddConcordance, Unit>
 {
-    /// <inheritdoc />
-    protected override IEnumerable<StepCase> StepCases
-    {
-        get
-        {
-            yield return new NuixStepCase(
-                "Add Concordance Test",
-                new NuixAddConcordance
-                {
-                    ConcordanceProfileName = Constant("IntegrationTestProfile"),
-                    ConcordanceDateFormat  = Constant("yyyy-MM-dd'T'HH:mm:ss.SSSZ"),
-                    FilePath               = ConcordancePath,
-                    Custodian              = Constant("Mark"),
-                    Container              = Constant("New Folder"),
-                    CasePath               = CasePath,
-                },
-                Unit.Default,
-                new List<ExternalProcessAction>
-                {
-                    new(
-                        new ConnectionCommand
-                        {
-                            Command            = "AddConcordanceToCase",
-                            FunctionDefinition = "",
-                            Arguments = new Dictionary<string, object>
-                            {
-                                { nameof(NuixAddConcordance.CasePath), CasePathString },
-                                { nameof(NuixAddConcordance.Container), "New Folder" },
-                                { nameof(NuixAddConcordance.Custodian), "Mark" },
-                                {
-                                    nameof(NuixAddConcordance.FilePath),
-                                    ConcordancePathString
-                                },
-                                {
-                                    nameof(NuixAddConcordance.ConcordanceDateFormat),
-                                    "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-                                },
-                                {
-                                    nameof(NuixAddConcordance.ConcordanceProfileName),
-                                    "IntegrationTestProfile"
-                                }
-                            }
-                        },
-                        new ConnectionOutput
-                        {
-                            Result = new ConnectionOutputResult() { Data = null }
-                        }
-                    )
-                }
-            ).WithSettings(UnitTestSettings);
-        }
-    }
-
-    /// <inheritdoc />
-    protected override IEnumerable<DeserializeCase> DeserializeCases
-    {
-        get
-        {
-            string concordanceDateFormat = "yyyy-MM-dd''T''HH:mm:ss.SSSZ";
-
-            var integrationTestProfile = @"IntegrationTestProfile";
-            var custodian              = @"Mark";
-            var newFolder              = @"New Folder";
-
-            yield return new NuixDeserializeTest(
-                    "Add Concordance",
-                    $@"NuixAddConcordance CasePath: '{CasePathString}' ConcordanceDateFormat: '{concordanceDateFormat}' ConcordanceProfileName: '{integrationTestProfile}' Custodian: '{custodian}' FilePath: '{ConcordancePathString}' FolderName: '{newFolder}'",
-                    Unit.Default,
-                    new List<ExternalProcessAction>
-                    {
-                        new(
-                            new ConnectionCommand
-                            {
-                                Command = "AddConcordanceToCase",
-                                Arguments = new Dictionary<string, object>
-                                {
-                                    { nameof(NuixAddConcordance.CasePath), CasePathString },
-                                    { nameof(NuixAddConcordance.Container), newFolder },
-                                    { nameof(NuixAddConcordance.Custodian), custodian },
-                                    {
-                                        nameof(NuixAddConcordance.FilePath),
-                                        ConcordancePathString
-                                    },
-                                    {
-                                        nameof(NuixAddConcordance.ConcordanceDateFormat),
-                                        concordanceDateFormat.Replace("''", "'")
-                                    },
-                                    {
-                                        nameof(NuixAddConcordance.ConcordanceProfileName),
-                                        integrationTestProfile
-                                    }
-                                }
-                            },
-                            new ConnectionOutput
-                            {
-                                Result = new ConnectionOutputResult { Data = null }
-                            }
-                        )
-                    }
-                ).WithSettings(UnitTestSettings)
-                .WithFileAction(x => x.Setup(f => f.Exists(It.IsAny<string>())).Returns(true));
-        }
-    }
-
     /// <inheritdoc />
     protected override IEnumerable<NuixIntegrationTestCase> NuixTestCases
     {
@@ -129,18 +22,18 @@ public partial class NuixAddConcordanceTests : NuixStepTestBase<NuixAddConcordan
                 new NuixAddConcordance
                 {
                     FilePath               = ConcordancePath,
-                    Container              = Constant("New Folder"),
+                    Container              = Constant("ConcordanceTest"),
                     ConcordanceProfileName = Constant("IntegrationTestProfile"),
                     ConcordanceDateFormat  = Constant("yyyy-MM-dd'T'HH:mm:ss.SSSZ"),
                     Custodian              = Constant("Mark"),
                     Description            = Constant("Container description"),
                     ContainerEncoding      = Constant("UTF-8"),
-                    ContainerLocale        = Constant("en_GB"),
+                    ContainerLocale        = Constant("en-GB"),
                     ContainerTimeZone      = Constant("UTC")
                 },
-                AssertCount(2, "*.txt"),
-                new NuixCloseConnection(),
-                DeleteCaseFolder
+                AssertCount(3, "*"),
+                AssertCount(1, "abandon"),
+                CleanupCase
             );
 
             yield return new NuixIntegrationTestCase(
@@ -150,15 +43,16 @@ public partial class NuixAddConcordanceTests : NuixStepTestBase<NuixAddConcordan
                 new NuixAddConcordance
                 {
                     FilePath               = ConcordancePath,
-                    Container              = Constant("New Folder"),
+                    Container              = Constant("ConcordanceTest"),
                     ConcordanceProfileName = Constant("IntegrationTestProfile"),
+                    ContainerEncoding      = Constant("UTF-8"),
                     ProcessingSettings     = Constant(Entity.Create(("create_thumbnails", false))),
                     CustomMetadata         = Constant(Entity.Create(("CustomMeta", "value")))
                 },
-                AssertCount(2, "*.txt"),
-                AssertCount(2, "custom-metadata:\"CustomMeta\":*"),
-                new NuixCloseConnection(),
-                DeleteCaseFolder
+                AssertCount(3, "*"),
+                AssertCount(1, "abandon"),
+                AssertCount(3, "evidence-metadata:\"CustomMeta:*\""),
+                CleanupCase
             );
 
             yield return new NuixIntegrationTestCase(
@@ -168,14 +62,14 @@ public partial class NuixAddConcordanceTests : NuixStepTestBase<NuixAddConcordan
                 new NuixAddConcordance
                 {
                     FilePath               = ConcordancePath,
-                    Container              = Constant("New Folder"),
+                    Container              = Constant("ConcordanceTest"),
                     ConcordanceProfileName = Constant("IntegrationTestProfile"),
+                    ContainerEncoding      = Constant("UTF-8"),
                     OpticonPath            = OpticonPath
                 },
-                AssertCount(2, "*"),
-                AssertCount(2, "*.txt"),
-                new NuixCloseConnection(),
-                DeleteCaseFolder
+                AssertCount(3, "*"),
+                AssertCount(1, "abandon"),
+                CleanupCase
             );
         }
     }
