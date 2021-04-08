98d2257450cade97ef75dffb91f0e40b27124bf7
antonyoni <antony@reduc.tech>
2021-04-08 12:06:23 +0100

    Add NuixGetVersion step and tests Closes #188

---
+++ Nuix.Tests/Steps/NuixGetVersionTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Reductech.EDR.Connectors.Nuix.Steps;
+using Reductech.EDR.Core;
+using Reductech.EDR.Core.Steps;
+using static Reductech.EDR.Core.TestHarness.StaticHelpers;
+using static Reductech.EDR.Connectors.Nuix.Tests.Constants;
+
+namespace Reductech.EDR.Connectors.Nuix.Tests.Steps
+{
+
+public partial class NuixGetVersionTests : NuixStepTestBase<NuixGetVersion, StringStream>
+{
+    /// <inheritdoc />
+    protected override IEnumerable<NuixIntegrationTestCase> NuixTestCases
+    {
+        get
+        {
+            yield return new NuixIntegrationTestCase(
+                "Return Nuix version",
+                DeleteCaseFolder,
+                CreateCase,
+                new AssertTrue
+                {
+                    Boolean = new StringContains
+                    {
+                        String = new NuixGetVersion(),
+                        Substring = new EntityGetValue<StringStream>
+                        {
+                            Entity = new GetSettings(), Property = Constant("Version")
+                        }
+                    }
+                },
+                new NuixCloseConnection(),
+                DeleteCaseFolder
+            );
+        }
+    }
+}
+
+}
