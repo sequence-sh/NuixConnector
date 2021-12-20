cddc00948c94e509053be3bbd244c4f5a905c30e
antonyoni <antony@reduc.tech>
2021-12-20 17:08:17 +0000

    Update namespce

--- Nuix/Errors/ErrorMessages-EN.Designer.cs
+++ Nuix/Errors/ErrorMessages-EN.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Reductech.EDR.Connectors.Nuix.Errors {
+namespace Reductech.Sequence.Connectors.Nuix.Errors {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Reductech.EDR.Connectors.Nuix.Errors {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Reductech.EDR.Connectors.Nuix.Errors.ErrorMessages-EN", typeof(ErrorMessages_EN).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Reductech.Sequence.Connectors.Nuix.Errors.ErrorMessages-EN", typeof(ErrorMessages_EN).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
